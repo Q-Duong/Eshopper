2bc0bd0786440f4ec8b451df8fa235aa43fd7a14
Duong <huynhquocduong12345@gmail.com>
2021-12-16 11:10:32 +0700

    them title tu nhanh

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <link href="css/animate.css" rel="stylesheet">
 	<link href="css/main.css" rel="stylesheet">
 	<link href="css/responsive.css" rel="stylesheet">
-	
+	<title>them tu nhanh</title>
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
